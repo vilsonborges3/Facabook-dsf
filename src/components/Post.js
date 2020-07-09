da5d41d7a07663d6ce151127c813c1bc39d05f9d
Vilson Borges <vilsonborges98@gmail.com>
2020-07-09 10:24:39 -0300

    add arquivo dos comentarios

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import '../App.css'
+import Comment from './Comment';
 
 function Post({ post }){
     return (
         <div id='post'>
-            <div id='divLogo'>
-                <img src={post.author.avatar} id='logo'/> 
-            </div>  
+            <div id='photoAndName'>
+                <div id='divLogo'>
+                    <img src={post.author.avatar} id='logo'/> 
+                </div>  
 
-            <div id='nameAndDate'>
-                <p>
-                    <strong >{post.author.name}<br></br></strong>
-                        
-                    <small id='textdate'>{post.date}</small> 
-                </p>
+                <div id='nameAndDate'>
+                    <p>
+                        <strong >{post.author.name}<br></br></strong>
+                            
+                        <small id='textdate'>{post.date}</small> 
+                    </p>
+                </div>
             </div>
-            <p>
-                {post.content}
-            </p>
-            
+            {post.content}
+            {
+                post.comments.map( comment => 
+                    <Comment 
+                        key={comment.id}
+                        comment={comment}
+                    />
+                    )
+            }
         </div>         
     )
 }
